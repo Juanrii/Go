bc447e1de95755693e84b5c01d6b2256eb7f7d2b
Juan Ignacio Rios <juanrios0495@gmail.com>
2021-03-06 14:45:37 -0300

    Variables and fmt package

--- src/main.go
+++ src/main.go
@@ -2,7 +2,39 @@ package main
 
 import "fmt"
 
-func main() {
-	fmt.Println("Hola mundo")
-	
+func calcularArea(base, altura int) {
+	result := base * altura
+	fmt.Println(result)
 }
+
+func main() {
+	// Constantes
+	const pi float64 = 3.14
+	const pi2 = 3.14
+	fmt.Println(pi, pi2)
+
+	// Variables enteras
+	base := 12 // := declara y asgina
+	var altura int = 14
+	var area int
+
+	fmt.Println(base, altura, area)
+
+	// Zero values
+	var a int
+	var b float64
+	var c string
+	var d bool
+
+	fmt.Println(a,b,c,d)
+
+	// Sprintf
+	name := "Juan"
+	message := fmt.Sprintf("Hi, I'm %s", name)
+	fmt.Println(message)
+
+	// Tipo de dato %T
+	fmt.Printf("name: %T", name)
+
+	calcularArea(2,4)
+}	
